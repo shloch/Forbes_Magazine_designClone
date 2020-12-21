89e2d3945bede1bc13642006a8e482ed97f64162
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2020-12-21 13:32:03 +0100

    adding LOGO + NEWS SLIDE

--- media/js/myscript.js
+++ media/js/myscript.js
@@ -1,3 +1,33 @@
-'use strict'
+var carousel = {
+  slides: document.querySelectorAll(".carousel__slide"),
+  currentSlide: 0,
+  next: document.querySelector(".nextArrow"),
+  prev: document.querySelector(".prevArrow")
+};
 
-alert('Welcome to Forbes');
+carousel.next.addEventListener("click", function (e) {
+  nextSlide(carousel);
+});
+carousel.prev.addEventListener("click", function (e) {
+  previousSlide(carousel);
+});
+
+function resetCurrentSlide() {
+  carousel.slides[carousel.currentSlide].classList.add('hideSlide');
+}
+
+function nextSlide(carousel) {
+  resetCurrentSlide();
+  carousel.currentSlide = (carousel.currentSlide + 1) % carousel.slides.length;
+  carousel.slides[carousel.currentSlide].classList.remove('hideSlide');
+}
+
+function previousSlide(carousel) {
+  resetCurrentSlide();
+
+  if (carousel.currentSlide === 0)
+    carousel.currentSlide = carousel.slides.length - 1;
+  else carousel.currentSlide--;
+
+  carousel.slides[carousel.currentSlide].classList.remove('hideSlide');
+}
